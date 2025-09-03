2f563ce95654812e0b4c2f6fb73de896ebccba51
emr4nbaloch <emr4nbaloch@gmail.com>
2025-09-03 04:58:28 +0330

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -11,9 +11,9 @@ logging.basicConfig(
 
 class Config(object):
     
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", ))
-    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8121525775:AAF2FvK-x3gZxlmQH_74uv3f4fjT9M6c4uo")
+    API_ID = int(os.environ.get("API_ID", 21814172))
+    API_HASH = os.environ.get("API_HASH", "9ab4911033c2451b80703f3612f3ac3d")
     
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     MAX_FILE_SIZE = 2194304000
@@ -30,16 +30,16 @@ class Config(object):
 
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
 
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://emr4nbaloch_db_user:q26oNqVpM0qeh9I5@cluster0.errbpya.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002909766283"))
     LOGGER = logging
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "7623433716"))
     SESSION_NAME = "UploaderXNTBot"
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002909766283")
 
     TG_MIN_FILE_SIZE = 2194304000
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "MovieesgramBot")
     ADL_BOT_RQ = {}
 
     # Set False off else True
